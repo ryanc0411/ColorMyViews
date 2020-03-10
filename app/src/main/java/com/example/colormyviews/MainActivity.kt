ea371a2841e7163abc4fba890816190657041d11
Ryan <ryanc0411@gmail.com>
2020-03-10 11:37:58 +0800

    first commit

---
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.example.colormyviews
+
+import android.graphics.Color
+import android.graphics.Color.*
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setListeners()
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for the background
+            R.id.box_one_text -> view.setBackgroundColor(DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(GRAY)
+            R.id.box_three_text -> view.setBackgroundColor(BLUE)
+            R.id.box_four_text -> view.setBackgroundColor(MAGENTA)
+            R.id.box_five_text -> view.setBackgroundColor(BLUE)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+
+    }
+
+    private fun setListeners() {
+
+        val boxOneText = findViewById<TextView>(R.id.box_one_text)
+        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
+        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
+        val boxFourText = findViewById<TextView>(R.id.box_four_text)
+        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
+
+        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
+
+        val clickableViews: List<View> =
+            listOf(
+                boxOneText, boxTwoText, boxThreeText,
+                boxFourText, boxFiveText, rootConstraintLayout
+            )
+
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) ;setBackgroundResource(it) }
+
+        }
+    }
+
+    private fun setBackgroundResource(view: View) {
+        when(view.id) {
+            R.id.box_two_text -> view.setBackgroundResource(R.drawable.ic_launcher_background)
+        }
+    }
+}
